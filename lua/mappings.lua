e3076a438f3f23131a74b63820485b2e60dfaff9
Mateus Gurgel <mateusgurgel15@gmail.com>
2025-10-21 00:36:36 -0300

    feat: add multi select on telescope

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,11 +4,37 @@ vim.g.mapleader = " "
 
 -- Telescope
 
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
 local builtin = require "telescope.builtin"
-vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<C-f>", builtin.live_grep, { desc = "Live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "List buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+
+local function find_files_multi()
+  builtin.find_files {
+    attach_mappings = function(prompt_bufnr, map)
+      local function open_selected_files()
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        local multi = picker:get_multi_selection()
+        if vim.tbl_isempty(multi) then
+          actions.select_default(prompt_bufnr)
+        else
+          actions.close(prompt_bufnr)
+          for _, entry in ipairs(multi) do
+            vim.cmd("edit " .. vim.fn.fnameescape(entry.path))
+          end
+        end
+      end
+
+      map("i", "<CR>", open_selected_files)
+      map("n", "<CR>", open_selected_files)
+      return true
+    end,
+  }
+end
+
+vim.keymap.set("n", "<C-p>", find_files_multi, { desc = "Find files (multi-select <CR>)" })
 
 -- Terminals
 
